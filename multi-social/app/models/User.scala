eaf54e0dd904df0ed68f60e467f66f0caca74d10
Rajish <rgaller@gazeta.pl>
2012-02-14 17:19:37 +0100

    WIP: simple example Play!

---
+++ multi-social/app/models/User.scala
@@ -0,0 +1,12 @@
+package models
+
+import java.util.{Date}
+
+import play.api.db._
+import play.api.Play.current
+
+import anorm._
+import anorm.SqlParser._
+
+class User(name: String, pass: String) {
+}
