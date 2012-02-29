e25302a27935fd3bdcb4d44d4f9684101e7cf3a6
Radzisław Galler <rgaller@gazeta.pl>
2012-03-01 00:51:27 +0100

    Tweaks with project configurations for ensime

--- dinobase/project/Build.scala
+++ dinobase/project/Build.scala
@@ -23,6 +23,11 @@ object ApplicationBuild extends Build {
           "models\\..*",
           "views\\..*",
           "play\\..api\\..*"
+        ),
+        key(":source-roots"), sexp(
+          "/home/rajish/proj/scala-beginner/dinobase/app",
+          "/home/rajish/proj/scala-beginner/dinobase/test",
+          "/home/rajish/bin/play2/framework/src"
         )
       )
     )
